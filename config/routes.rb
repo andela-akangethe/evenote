ac612d6f7f67cfeb6727cbfba688084ee0d3b311
andela-akangethe <alex.kangethe@andela.com>
2016-04-13 18:37:33 +0300

    Adds docs views and defines actions

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
-  resource :docs
+  resources :docs
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
